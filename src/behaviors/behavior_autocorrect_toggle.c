79241e3f6cf3205038c4430456beb711214159a7
wlo2 <n2q@protonmail.com>
2025-10-12 17:48:56 +0300

    fix 21.5

--- src/behaviors/behavior_autocorrect_toggle.c
+++ src/behaviors/behavior_autocorrect_toggle.c
@@ -8,6 +8,7 @@
 
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
+#include <zmk/behavior.h>
 #include <zmk/autocorrect.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -15,11 +16,26 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 static int behavior_autocorrect_toggle_init(const struct device *dev) {
-    autocorrect_toggle();
     return 0;
 }
 
-DEVICE_DT_INST_DEFINE(0, behavior_autocorrect_toggle_init, NULL, NULL, NULL, 
-                      POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY, NULL);
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    autocorrect_toggle();
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static const struct behavior_driver_api behavior_autocorrect_toggle_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+};
+
+BEHAVIOR_DT_INST_DEFINE(0, behavior_autocorrect_toggle_init, NULL, NULL, NULL, POST_KERNEL,
+                        CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_autocorrect_toggle_driver_api);
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
